7403aad28eb07588e18e1c50ad36ef2bc5a1a714
Godfred Asamoah <realswat@meta.com>
2023-06-14 11:39:13 -0700

    submit button

---
+++ script.js
@@ -0,0 +1,8 @@
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent form submission (optional)
+
+    // Perform your desired action here
+    console.log("Button clicked!");
+});
